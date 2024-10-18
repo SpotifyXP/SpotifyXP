332391dd56b5b8940648f728fab269b29df22202
werwolf2303 <66752411+werwolf2303@users.noreply.github.com>
2024-10-18 21:55:55 +0200

    Implement mpris like tray icon for windows

--- src/main/java/com/spotifyxp/background/BackgroundService.java
+++ src/main/java/com/spotifyxp/background/BackgroundService.java
@@ -1,17 +1,25 @@
 package com.spotifyxp.background;
 
 
-import com.spotifyxp.dialogs.SystemTrayDialog;
+import com.spotifyxp.PublicValues;
+import com.spotifyxp.events.EventSubscriber;
+import com.spotifyxp.events.Events;
+import com.spotifyxp.events.SpotifyXPEvents;
+import com.spotifyxp.lib.libDetect;
+import com.spotifyxp.manager.InstanceManager;
+import com.spotifyxp.tray.ExtendedSystemTray;
+import com.spotifyxp.tray.SystemTrayDialog;
 import com.spotifyxp.logging.ConsoleLogging;
 import com.spotifyxp.panels.ContentPanel;
 import com.spotifyxp.utils.Resources;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
 
-public class BackgroundService {
+public class BackgroundService implements ExtendedSystemTray {
     /**
      * Holds the trayDialog after start() was called
      */
@@ -23,16 +31,92 @@ public class BackgroundService {
     public void start() {
         try {
             trayDialog = new SystemTrayDialog();
-            trayDialog.add(new ImageIcon(ImageIO.read(new Resources().readToInputStream("spotifyxp.png"))), "SpotifyXP");
-            trayDialog.open(new MouseAdapter() {
-                @Override
-                public void mouseClicked(MouseEvent e) {
-                    ContentPanel.frame.setVisible(true);
-                    ContentPanel.frame.requestFocus();
-                }
-            });
+            trayDialog.setExtendedTrayIcon(this, e -> {
+                ContentPanel.frame.setVisible(true);
+                ContentPanel.frame.requestFocus();
+            }, new ImageIcon(ImageIO.read(new Resources().readToInputStream("spotifyxp.png"))).getImage(), "SpotifyXP");
+            trayDialog.openExtended();
         } catch (Exception e) {
             ConsoleLogging.Throwable(e);
         }
+    }
+
+    @Override
+    public void onInit(JDialog dialog) {
+        if(PublicValues.osType != libDetect.OSType.Windows) {
+            dialog.dispose();
+            return;
+        }
+        JPanel contentPanel = new JPanel();
+        contentPanel.setLayout(new BorderLayout());
+
+        JPanel textPanel = new JPanel();
+        contentPanel.setLayout(new BorderLayout());
+
+        try {
+            JTextField titleAndText = new JTextField();
+            titleAndText.setEditable(false);
+
+            titleAndText.setText(InstanceManager.getPlayer().getPlayer().currentMetadata().getName() + " - "
+                    + InstanceManager.getPlayer().getPlayer().currentMetadata().getArtist());
+
+            textPanel.add(titleAndText, BorderLayout.CENTER);
+        }catch (NullPointerException e) {
+            ConsoleLogging.Throwable(e);
+            JTextField titleAndText = new JTextField("N/A");
+            titleAndText.setEditable(false);
+
+            textPanel.add(titleAndText,BorderLayout.CENTER);
+        }
+
+        contentPanel.add(textPanel, BorderLayout.NORTH);
+
+        JPanel controlPanel = new JPanel();
+        controlPanel.setLayout(new BorderLayout());
+
+        JButton nextButton = new JButton("Next"); //ToDo: Translate
+        JButton previousButton = new JButton("Previous"); //ToDo: Translate
+        JButton playPauseButton = new JButton("P/P"); //ToDo: Translate
+
+        nextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                InstanceManager.getPlayer().getPlayer().next();
+            }
+        });
+
+        playPauseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                InstanceManager.getPlayer().getPlayer().playPause();
+            }
+        });
+
+        previousButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                InstanceManager.getPlayer().getPlayer().previous();
+            }
+        });
+
+        controlPanel.add(nextButton, BorderLayout.EAST);
+        controlPanel.add(playPauseButton, BorderLayout.CENTER);
+        controlPanel.add(previousButton, BorderLayout.WEST);
+
+        contentPanel.add(controlPanel, BorderLayout.SOUTH);
+
+        dialog.setContentPane(contentPanel);
+    }
+
+    @Override
+    public void onOpen() {
+        if(PublicValues.osType != libDetect.OSType.Windows) {
+            ContentPanel.frame.setVisible(true);
+            ContentPanel.frame.requestFocus();
+        }
+    }
+
+    @Override
+    public void onClose() {
     }
 }
---
+++ src/main/java/com/spotifyxp/tray/ExtendedSystemTray.java
@@ -0,0 +1,9 @@
+package com.spotifyxp.tray;
+
+import javax.swing.*;
+
+public interface ExtendedSystemTray {
+    void onInit(JDialog dialog);
+    void onOpen();
+    void onClose();
+}
